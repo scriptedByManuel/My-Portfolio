fd5e80d91f66636a07572e4e032d785e1abe1e71
Manuel <antthtoonaing74@gmail.com>
2025-11-18 14:55:44 +0630

    Second Commit

--- components/projects.jsx
+++ components/projects.jsx
@@ -21,7 +21,7 @@ const projects = [
     image: "/invoice-app.png",
     tags: ["React", "Tailwind CSS", "API", "Responsive", "SWR", "React Router", "React Hook Form"],
     github: "https://github.com/scriptedByManuel/Invoice-App",
-    demo: "https://pixelsolutionsmm.netlify.app/",
+    demo: "https://devpixelsolutions.netlify.app/",
   },
 ]
 
